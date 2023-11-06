448cc95ea857b21f80e1f5f5d690219c0e23f20a
Jeffrey Wright <jeffrey.wright@wartsila.com>
2023-11-06 15:26:58 -0700

    Fix hooks spec to match calling code and provide Results object.

--- pytest_oof/hooks.py
+++ pytest_oof/hooks.py
@@ -10,7 +10,7 @@ from .utils import Results
 
 class HookSpecs:
     @pytest.hookspec(firstresult=True)
-    def pytest_oof_results(self) -> Results:
+    def pytest_oof_results(self, results: Results) -> Results:
         """
         Called after the test session is finished to provide a Results
         instance with the collected test session data.
