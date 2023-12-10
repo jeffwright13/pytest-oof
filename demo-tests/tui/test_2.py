dbb0fc3c4f234715ef3a4d5d0bc3f39c3090c342
Jeffrey Wright <jeffrey.wright@wartsila.com>
2023-12-10 16:06:12 -0700

    Issue1 - fixed.

--- demo-tests/tui/test_2.py
+++ demo-tests/tui/test_2.py
@@ -28,29 +28,28 @@ def test_a_ok(fake_data, logger):
 
 def test_b_fail(fake_data, logger):
     logger.critical(fake_data)
-    logger.error(fake_data())
-    logger.warning(fake_data())
-    logger.info(fake_data())
-    logger.debug(fake_data())
+    logger.error(fake_data)
+    logger.warning(fake_data)
+    logger.info(fake_data)
+    logger.debug(fake_data)
     assert 0
 
 
 def test_c_error(fake_data, error_fixture, logger):
     print("This test should be marked as an Error.")
     logger.critical(fake_data)
-    logger.error(fake_data())
-    logger.warning(fake_data())
-    logger.info(fake_data())
-    logger.debug(fake_data())
-    pass
+    logger.error(fake_data)
+    logger.warning(fake_data)
+    logger.info(fake_data)
+    logger.debug(fake_data)
 
 
 def test_d1_skip(fake_data, logger):
     logger.critical(fake_data)
-    logger.error(fake_data())
-    logger.warning(fake_data())
-    logger.info(fake_data())
-    logger.debug(fake_data())
+    logger.error(fake_data)
+    logger.warning(fake_data)
+    logger.info(fake_data)
+    logger.debug(fake_data)
     pytest.skip("Skipping this test with inline call to 'pytest.skip()'.")
 
 
@@ -59,42 +58,42 @@ pytest.mark.skip(reason="Skipping this test with decorator.")
 
 def test_d2_skip(fake_data, logger):
     logger.critical(fake_data)
-    logger.error(fake_data())
-    logger.warning(fake_data())
-    logger.info(fake_data())
-    logger.debug(fake_data())
+    logger.error(fake_data)
+    logger.warning(fake_data)
+    logger.info(fake_data)
+    logger.debug(fake_data)
 
 
 def test_d3_skip(fake_data, logger):
     logger.critical(fake_data)
-    logger.error(fake_data())
-    logger.warning(fake_data())
-    logger.info(fake_data())
-    logger.debug(fake_data())
+    logger.error(fake_data)
+    logger.warning(fake_data)
+    logger.info(fake_data)
+    logger.debug(fake_data)
     pytest.skip("Skipping this test with inline call to 'pytest.skip()'.")
 
 
 def test_e1(fake_data, logger):
     logger.critical(fake_data)
-    logger.error(fake_data())
-    logger.warning(fake_data())
-    logger.info(fake_data())
-    logger.debug(fake_data())
+    logger.error(fake_data)
+    logger.warning(fake_data)
+    logger.info(fake_data)
+    logger.debug(fake_data)
 
 
 @pytest.mark.xfail(reason="Marked as Xfail with decorator.")
 def test_e2(fake_data, logger):
     logger.critical(fake_data)
-    logger.error(fake_data())
-    logger.warning(fake_data())
-    logger.info(fake_data())
-    logger.debug(fake_data())
+    logger.error(fake_data)
+    logger.warning(fake_data)
+    logger.info(fake_data)
+    logger.debug(fake_data)
 
 
 def test_f1(fake_data, logger):
     logger.critical(fake_data)
-    logger.error(fake_data())
-    logger.warning(fake_data())
-    logger.info(fake_data())
-    logger.debug(fake_data())
+    logger.error(fake_data)
+    logger.warning(fake_data)
+    logger.info(fake_data)
+    logger.debug(fake_data)
     assert True
