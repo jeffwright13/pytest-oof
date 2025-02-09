26b535a75889fc80b11ba3a6faf238b2f6f6a8f7
Jeffrey Wright <jeffrey.wright@wartsila.com>
2025-02-08 22:08:54 -0700

    Add oof console script.

--- setup.py
+++ setup.py
@@ -65,11 +65,10 @@ setup(
     keywords="pytest pytest-plugin testing",
     entry_points={
         "console_scripts": [
-            "oofda = pytest_oof.__main__:main",
+            "oof = pytest_oof.__main__:main",
             "oof-console = pytest_oof.clients.console_gen:main",
             "oof-html = pytest_oof.clients.html_gen:main",
             "oof-tui = pytest_oof.clients.tui_gen:main",
-            "oof-analyze = pytest_oof.analyze_results:main",
             "oof-gui = pytest_oof.gui.longitudinal_view:main",
         ],
         "pytest11": [
